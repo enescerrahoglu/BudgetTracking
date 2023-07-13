3e3893963c6a80979454070e774b5abde0725247
Enes Cerrahoğlu <enescerrahoglu1@gmail.com>
2023-07-13 14:57:01 +0300

    geliştirmeler yapıldı.

--- app/src/main/java/com/enescerrahoglu/budgettracking/user/ProfileFragment.kt
+++ app/src/main/java/com/enescerrahoglu/budgettracking/user/ProfileFragment.kt
@@ -6,11 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.enescerrahoglu.budgettracking.databinding.FragmentProfileBinding
+import com.google.gson.Gson
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+    private lateinit var userViewModel: UserViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +30,25 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.currentUserPhoneNumberLL.setOnClickListener {
-            activity?.let{
-                val intent = Intent (it, UpdateProfileActivity::class.java)
-                it.startActivity(intent)
+        userViewModel = ViewModelProvider(requireActivity())[UserViewModel::class.java]
+
+        observeData()
+    }
+
+    private fun observeData(){
+        userViewModel.userModel.observe(this, Observer { data ->
+            if(data != null){
+                binding.currentUserPhoneNumberTextView.text = "${data.firstName} ${data.lastName}"
+                binding.currentUserPhoneNumberLL.setOnClickListener {
+                    activity?.let{
+                        val intent = Intent (it, UpdateProfileActivity::class.java)
+                        val gson = Gson()
+                        val json = gson.toJson(data)
+                        intent.putExtra("userModel", json)
+                        it.startActivity(intent)
+                    }
+                }
             }
-        }
+        })
     }
 }
--- app/src/main/java/com/enescerrahoglu/budgettracking/user/UpdateProfileActivity.kt
+++ app/src/main/java/com/enescerrahoglu/budgettracking/user/UpdateProfileActivity.kt
@@ -1,18 +1,66 @@
 package com.enescerrahoglu.budgettracking.user
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
+import com.enescerrahoglu.budgettracking.NavigationActivity
 import com.enescerrahoglu.budgettracking.databinding.ActivityUpdateProfileBinding
+import com.google.gson.Gson
 
 class UpdateProfileActivity : AppCompatActivity() {
+    private lateinit var viewModel: UserViewModel
+
     private lateinit var binding: ActivityUpdateProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUpdateProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        viewModel = ViewModelProviders.of(this)[UserViewModel::class.java]
 
         supportActionBar?.let {
             supportActionBar!!.title = "Update Profile"
+        }
+        val userExtra = intent.getStringExtra("userModel")
+        val gson = Gson()
+        val userFromJson = gson.fromJson(userExtra, UserModel::class.java)
+        viewModel.setUser(userFromJson)
+        binding.phoneNumberEditText.setText(userFromJson.phoneNumber)
+        binding.firstNameEditText.setText(userFromJson.firstName)
+        binding.lastNameEditText.setText(userFromJson.lastName)
+        userFromJson.monthlyBudget?.let {
+            binding.monthlyBudgetEditText.setText(userFromJson.monthlyBudget!!.toString())
+        }
+        binding.updateProfileButton.setOnClickListener {
+            updateUser()
+        }
+    }
+
+    private fun updateUser(){
+        val firstName = binding.firstNameEditText.text.toString()
+        val lastName =binding.lastNameEditText.text.toString()
+        val monthlyBudget = binding.monthlyBudgetEditText.text.toString()
+        if(firstName.isNotEmpty()  && lastName.isNotEmpty() && monthlyBudget.isNotEmpty() ){
+            viewModel.userModel.value?.let {
+                val user = UserModel(viewModel.userModel.value!!.phoneNumber,firstName, lastName, monthlyBudget.toDouble())
+                viewModel.setUser(
+                    user
+                )
+                viewModel.updateUser { isSuccessful ->
+                    if(isSuccessful){
+                        val intent = Intent(this, NavigationActivity::class.java)
+                        val user = viewModel.userModel.value
+                        val gson = Gson()
+                        val json = gson.toJson(user)
+                        intent.putExtra("userModel", json)
+                        startActivity(intent)
+                        finish()
+                        println("isSuccessful")
+                    }
+                }
+            }
+        }else{
+            println("false")
         }
     }
 }
